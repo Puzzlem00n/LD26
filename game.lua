3752bb743ca6d0adef9594169715b115d2c4ecc7
Tim B. <werpuzzlemoon@gmail.com>
2013-04-27 18:25:18 -0400

    Made color switching actually work.
    Well, you know, minus the status effects.

--- game.lua
+++ game.lua
@@ -18,6 +18,28 @@ ylev = 1
 
 function game.update()
 	player:update()
+	bump.collide()
+	for i, pickup in pairs(pickups) do
+		pickup:update()
+	end
+end
+
+function bump.shouldCollide(i1, i2)
+	if instanceOf(Player, i1) or instanceOf(Player,i2) then
+		return true
+	end
+end
+
+function bump.collision(i1, i2, dx, dy)
+	if instanceOf(Pickup,i1) then
+		if i2.alpha ~= 0 then
+			player.color = i1.color
+		end
+	elseif instanceOf(Pickup,i2) then
+		if i2.alpha ~= 0 then
+			player.color = i2.color
+		end
+	end
 end
 
 function loadLevel(u, v)
--- pickup.lua
+++ pickup.lua
@@ -6,6 +6,7 @@ function Pickup:initialize(x, y, col)
 	self.t = y + 5
 	self.w = 10
 	self.h = 10
+	self.alpha = 255
 	bump.add(self)
 	if col == 1 then
 		self.color = colors.red
@@ -16,9 +17,17 @@ function Pickup:initialize(x, y, col)
 	end
 end
 
+function Pickup:update()
+	if player.color == self.color then
+		self.alpha = 0
+	else
+		self.alpha = 255
+	end
+end
+
 function Pickup:draw()
-	love.graphics.setColor(self.color[1], self.color[2], self.color[3])
-	love.graphics.draw(imgPickup, self.l, self.t)
+	love.graphics.setColor(self.color[1], self.color[2], self.color[3], self.alpha)
+	love.graphics.draw(imgPickup, self.l - 5, self.t - 5)
 	love.graphics.setColor(255, 255, 255)
 end
 
