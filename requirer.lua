8e2cec9275486ef493010aabb1bd8f8c22eec60d
Tim B. <werpuzzlemoon@gmail.com>
2013-04-26 20:03:32 -0400

    Woops.

--- requirer.lua
+++ requirer.lua
@@ -4,6 +4,7 @@ bump = require "lib.bump"
 anim8 = require "lib.anim8"
 camera = require "lib.camera"
 tween = require "lib.tween"
+Tserial = require "lib.Tserial"
 require "lib.middleclass"
 arc_path = 'Navi.arc.'
 require(arc_path .. 'arc')
